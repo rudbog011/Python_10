04f63487b8a0f43d76cb846a96c561a6225fc5f9
rudbog011 <rudbog011@gmail.com>
2021-06-01 18:19:47 +0300

    Исправление

--- work_10_8.py
+++ work_10_8.py
@@ -1,7 +1,7 @@
 print('Введите строку:')
 s = input()
 count = 0
-for i in range(len(s)):
-    if s[i] == s[i - 1]:
+for i in range(len(s) - 1):
+    if s[i] == s[i + 1]:
         count += 1
 print(count)
